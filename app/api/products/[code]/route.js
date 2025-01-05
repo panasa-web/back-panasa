1df1cb1c5bb5c56b5be2b9edaa3e66d539b5d56d
alonsoyiyi <angel.paz.consultas@gmail.com>
2025-01-05 17:17:10 -0500

    version 2

--- app/api/products/[code]/route.js
+++ app/api/products/[code]/route.js
@@ -4,10 +4,20 @@ import path from 'path';
 
 const productsFilePath = path.join(process.cwd(), 'data', 'products.json');
 
-export async function GET() {
+export async function GET(request, { params }) {
   try {
     const fileContents = await fs.readFile(productsFilePath, 'utf8');
     const products = JSON.parse(fileContents);
+    const productCode = params.code;
+
+    if (productCode) {
+      const product = products.find(p => p.código === productCode);
+      if (!product) {
+        return NextResponse.json({ message: 'Producto no encontrado' }, { status: 404 });
+      }
+      return NextResponse.json(product);
+    }
+
     return NextResponse.json(products);
   } catch (error) {
     console.error('Error al leer los productos:', error);
